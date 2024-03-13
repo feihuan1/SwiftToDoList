c788902fa8aab0fa5a10f742f75472d280c83e58
feihuan1 <ericrealtor1997@gmail.com>
2024-03-13 15:06:43 -0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Feihuan Peng on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
